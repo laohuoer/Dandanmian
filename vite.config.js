b424b3d4093a8a05539aed28f90f47a61f252408
LaoHuogym <51075681+laohuoer@users.noreply.github.com>
2026-06-22 17:44:36 +0800

    update: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Dandanmian/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [vue()],
   server: {
     port: 3000,
